b5a7952d2a15136aeff4a8be79fb83c1191d3cc6
Wisdom Praise <wisdompraise968@gmail.com>
2020-10-12 15:59:29 +0100

    Finished Course on Basic API development

--- basic_api/routes/api.php
+++ basic_api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// get all posts
+// Route::get('/posts',[PostController::class, 'index']);
+
+// // create route
+// Route::post('/posts', 'PostController@index');
+
+// // update
+// Route::put('/posts/{id}');
+
+// //delete
+// Route::delete('/posts/{id}');
+
+// //get a single post
+// Route::get('/posts/{id}');
+
+//short hand
+Route::prefix('v1')->group(function(){
+    Route::apiResource('posts',PostController::class);
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
